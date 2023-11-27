5cc6d3dc4b83d8d1c42d979a4206b19a70c72655
racossin <racossin@svsu.edu>
2023-11-27 16:30:39 -0500

    Added schema for users, items, and garage sales as well as provided the mongodb connection string

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ const sellerRouters = require('./routers/sellerRouter');
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 
-
-
-
+ 
 //middle ware
 const app = express()
 //this starts the server and assignes its port
 const port = process.env.PORT
 app.listen(port)
 
+const url = process.env.MONGO_URL
 
+async function connect() {
+    try {
+        await mongoose.connect(url)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.error(error)
+    }
+}
 
+connect()
 
 app.use(clientRouters);
 app.use(sellerRouters);
