d9367a12b4df97929c4e51934e6057c270b9dc75
racossin <racossin@svsu.edu>
2023-11-30 20:44:32 -0500

    Added title to garage sale schema

--- models/GarageSale.js
+++ models/GarageSale.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const garageSaleSchema = new mongoose.Schema({
+    title:{type: String, default: "Garage Sale"},
     owner:{type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
     date: {type: Date, default: Date.now, required:true},
     address: {type: String, required:true},
