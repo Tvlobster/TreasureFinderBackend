3b1022cb7e38f17a28e624cc80c25e5f4dc3b919
racossin <racossin@svsu.edu>
2023-12-07 20:22:20 -0500

    Updated GarageSale Schema for times (num -> string)

--- models/GarageSale.js
+++ models/GarageSale.js
@@ -5,27 +5,12 @@ const garageSaleSchema = new mongoose.Schema({
     owner:{type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
     date: {type: Date, default: Date.now, required:true},
     address: {type: String, required:true},
-    startTime: {type:Number, required:true},
-    endTime: {type:Number, required:true},
+    startTime: {type:String, required:true},
+    endTime: {type:String, required:true},
     items: [
         {type: mongoose.Schema.Types.ObjectId, ref: 'Item'}
     ]
 })
-
-garageSaleSchema.virtual('User',{
-    ref:'User',
-    localField:'owner',
-    foreignField:'_id'
-})
-
-garageSaleSchema.virtual('Item',{
-    ref:'Item',
-    localField:'_id',
-    foreignField:'owner'
-})
-
-garageSaleSchema.set('toObject',{virtuals:true})
-garageSaleSchema.set('toJSON',{virtuals:true})
 
 const GarageSale = mongoose.model("GarageSale", garageSaleSchema, 'Garage_Sales')
 module.exports = GarageSale
